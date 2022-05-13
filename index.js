bba0ce560a1ef87c1455971df934e489fa2f41f9
kumabio <kumabio.prince@student.moringaschool.com>
2022-05-13 12:17:11 +0000

    Assignment Completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(funTime="roller-skate"){
+    return `This Saturday, I want to ${funTime}!`
+  }
+  
+  function mondayWork(task="go to the office"){
+    return `This Monday, I will ${task}.`
+  }
+  
+  function wrapAdjective(highlight){
+      return function newfunc(adjective){
+        return `You are ${highlight}${adjective}${highlight}!`
+      }
+  }
